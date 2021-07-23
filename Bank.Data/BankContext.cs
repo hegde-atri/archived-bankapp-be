3ebd5c8a5987de8c0f3447fab8a8db26d501ab62
hegde-atri <iamatrihegde@gmail.com>
2021-07-23 19:16:43 +0100

    Created entities and DbContext

---
+++ Bank.Data/BankContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bank.Data
+{
+    public class BankContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Payee> Payees { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Bank.Data/Entities/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bank.Data.Entities
+{
+  public class Transaction
+  {
+    public int TransactionId { get; set; }
+    public int AccountId { get; set; }
+    public string Type { get; set; }
+    public decimal Amount { get; set; }
+    public string Description { get; set; }
+    public DateTime TransDateTime { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string ModifiedBy { get; set; }
+    public DateTime ModifiedDate { get; set; }
+  }
+}
