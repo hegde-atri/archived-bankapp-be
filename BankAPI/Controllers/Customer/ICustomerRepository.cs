05378e600901f8ff2fb26e7d92f8d159dfa80a73
hegde-atri <iamatrihegde@gmail.com>
2021-07-24 23:09:39 +0100

    creating CustomerRepository.cs

---
+++ BankAPI/Controllers/Customer/ICustomerRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Bank.Data.Entities;
+
+namespace BankAPI.Controllers.Customer
+{
+  public interface ICustomerRepository
+  {
+    // This class will help me plan what actions this repository should make
+    // These should be actions that I as a customer should be able to make
+    
+    // General
+    void Add<T>(T entity) where T : class; //     Generic constraint specifying that T that is passed in
+    void Delete<T>(T entity) where T : class;//   must be an entity that I've made (user-defined class 
+    Task<bool> SaveChangesAsync();//              with parameter-less constructor). Adding a generic type constraint
+
+    // Regarding Accounts
+    Task<Account> GetAccountAsync(int accountId, int customerId, bool onlyActive = true);
+    Task<Account[]> GetAllAccountsAsync(int customerId, bool onlyActive = true);
+
+    // Regarding Addresses
+    Task<Account> GetAddressAsync(int addressId, int customerId, bool onlyActive = true);
+    Task<Account[]> GetAllAddressesAsync(int customerId, bool onlyActive = true);
+    
+    // Regarding Notifications
+    Task<Notification> GetNotificationAsync(string status, int customerId, bool onlyActive = true);
+    Task<Notification[]> GetAllNotificationsAsync(int customerId, bool onlyActive = true);
+    
+    // Regarding Payees
+    Task<Account> GetPayeeAsync(string name, int customerId, bool onlyActive = true);
+    Task<Account[]> GetAllPayeesAsync(int customerId, bool onlyActive = true);
+    
+    // Regarding Transactions
+    Task<Transaction> GetTransactionAsync();
+    Task<Transaction> GetTransactionsForAccountsAsync(Account[] accounts);
+
+  }
+}
---
+++ BankAPI/Models/AccountModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Bank.Data.Entities;
+
+namespace BankAPI.Models
+{
+  public class AccountModel
+  {
+    //These are the attributes of the Account Table.
+    public int AccountId { get; set; }
+    
+    //Each account belongs to a single customer. [CustomerId is a foreign key]
+    [Required]
+    public int CustomerId { get; set; }
+    
+    // This will be my surrogate key
+    [Required]
+    public string AccountNumber { get; set; }
+    
+    [Required]
+    [StringLength(maximumLength:8, MinimumLength = 8)]
+    // Sort code regular expression in XX-XX-XX where X is an integer
+    [RegularExpression(@"^(\d){2}-(\d){2}-(\d){2}$")]
+    public string Sortcode { get; set; }
+    
+    [Required]
+    public string Type { get; set; }
+    
+    [Required]
+    public decimal Balance { get; set; }
+    
+    [Required]
+    public string Status { get; set; }
+    
+    [Required]
+    public DateTime OpenDate { get; set; }
+
+    [Required]
+    public DateTime CloseDate { get; set; }
+    
+    [Required]
+    public string CreatedBy { get; set; }
+    
+    [Required]
+    public DateTime CreatedDate { get; set; }
+    
+    [Required]
+    public string ModifiedBy { get; set; }
+    
+    [Required]
+    public DateTime ModifiedDate { get; set; }
+    
+    
+    /*  These are the relationships that the Account table has other than the customer
+        Each Account can have many transactions  */
+    public ICollection<Transaction> Transactions { get; set; }
+  }
+}
