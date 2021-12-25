5f8664941a35c9734b94a86266d21a099a59d906
Atri Hegde <iamatrihegde@gmail.com>
2021-12-25 17:42:15 +0000

    updated db

---
+++ Bank.Data/Migrations/20211225173944_modifiedCustomer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bank.Data.Migrations
+{
+    public partial class modifiedCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Customers");
+        }
+    }
+}
